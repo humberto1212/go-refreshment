ce62f2bba3296e2f31b01701deb5ee8b3e6af13a
Humberto <humberto@Humbertos-Air.fritz.box>
2022-12-06 22:03:48 +0100

    create get single customer

--- handlefuncs/handlefuncs.go
+++ handlefuncs/handlefuncs.go
@@ -4,11 +4,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/humberto1212/go-refreshment/models"
 	"github.com/humberto1212/go-refreshment/psqlDb"
 )
 
+//===================
+// Get all Customers
+//===================
 func GetAllCustomers(w http.ResponseWriter, _ *http.Request) {
 	w.Header().Set("conten-type", "aplication/json")
 
@@ -16,11 +21,11 @@ func GetAllCustomers(w http.ResponseWriter, _ *http.Request) {
 
 	defer db.Close()
 
-	// check db
-	// errdb := db.Ping()
-	// if errdb != nil {
-	// 	panic(errdb)
-	// }
+	//check db
+	errdb := db.Ping()
+	if errdb != nil {
+		panic(errdb)
+	}
 
 	var customers []models.Customer
 
@@ -40,11 +45,62 @@ func GetAllCustomers(w http.ResponseWriter, _ *http.Request) {
 		}
 
 		customers = append(customers, customer)
+	}
+
+	errJson := json.NewEncoder(w).Encode(customers)
+	if errJson != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+}
+
+//============================
+// Getting a single customer
+//============================
+func GetSingleCustomer(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("conten-type", "aplication/json")
+
+	idStirng := mux.Vars(r)["id"]
+
+	id, err := strconv.Atoi(idStirng)
+
+	if err != nil {
+		fmt.Println("Error during conversion")
+		return
+	}
+
+	fmt.Println("++++++++++++++++///+>", id)
+
+	db := psqlDb.Connect()
+	defer db.Close()
+	//check db
+	errdb := db.Ping()
+	if errdb != nil {
+		panic(errdb)
+	}
+
+	customer := models.Customer{}
+
+	rows, err := db.Query(`SELECT id, name, role, email, phone, contacted  FROM customers where id=$1`, id)
+	if err != nil {
+
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		err = rows.Scan(&customer.ID, &customer.Name, &customer.Role, &customer.Email, &customer.Phone, &customer.Contacted)
+
+		if err != nil {
+			fmt.Println(err)
+		}
 
 	}
 
-	fmt.Println(customers)
-	errJson := json.NewEncoder(w).Encode(customers)
+	errJson := json.NewEncoder(w).Encode(customer)
 	if errJson != nil {
 		http.Error(w, err.Error(), 500)
 		return
